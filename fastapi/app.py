b07efbff55c1e4c0be34419b12862ede6de26c68
Syed Onais Ali Shah <onaisali4952@gmail.com>
2024-12-10 20:36:31 +0500

    Updated packages.json and app.py

--- fastapi/app.py
+++ fastapi/app.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.get("/")
+def main_func():
+    return {"message": "Hello World"}
